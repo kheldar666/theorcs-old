794e730db69315876306669162aaa451d61354c2
Martin Papy <martin.papy@gmail.com>
2017-01-24 15:00:17 +0700

    Adding Localisation

--- src/main/java/org/libermundi/theorcs/controllers/IndexController.java
+++ src/main/java/org/libermundi/theorcs/controllers/IndexController.java
@@ -1,11 +1,19 @@
 package org.libermundi.theorcs.controllers;
+import java.util.Calendar;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
  
 @Controller
 public class IndexController {
     @RequestMapping("/")
-    String index(){
+    String index(Model model){
+    	model.addAttribute("currentYear", getThisYear());
         return "index";
+    }
+    
+    private int getThisYear() {
+    	return Calendar.getInstance().get(Calendar.YEAR);
     }
 }
